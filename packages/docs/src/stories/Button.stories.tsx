2d38a50fe5195545931af4ee3efbde575344c86f
Rafael Ribeiro <rafaelribeiro12@live.com>
2023-06-05 18:10:40 -0300

    Configurado stories do Button

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,9 +1,19 @@
 import { StoryObj, Meta } from '@storybook/react'
-import { Button } from '@atlantis-ui/react'
+import { Button, ButtonProps } from '@atlantis-ui/react'
 
 export default {
   title: 'Button',
   component: Button,
-} as Meta
 
-export const Primary: StoryObj = {}
+  args: {
+    children: 'Enviar',
+  },
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {}
+
+export const Big: StoryObj<ButtonProps> = {
+  args: {
+    size: 'big',
+  },
+}
